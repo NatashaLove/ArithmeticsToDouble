3616b80283208dec7b08b5c9ec83efc0e0841ed1
Natasha S Love <debora@inbox.ru>
2018-10-12 01:03:43 -0500

    Update ArithmeticsToDouble.java
    working cast to double

--- ArithmeticsToDouble.java
+++ ArithmeticsToDouble.java
@@ -1,18 +1,21 @@
+import java.util.*;
+
 public class ArithmeticsToDouble {
 	public static void main (String [] args) {
 		
 		Scanner input= new Scanner(System.in);
 		int digit;
 		int digit2;
-		int sum;
-		sum= digit+digit2;
-		average= (double)sum/2; // (double) - casts int to double!
-		
+		int sum=0;
+				
 		System.out.println("Please enter an integer:");
 		digit= input.nextInt();
 		
 		System.out.println("Please enter another integer:");
 		digit2= input.nextInt();
+		
+		sum= digit+digit2;
+		double average= (double)sum/2; // (double) - casts int to double!
 		
 		System.out.println(digit +" + " +digit2+ " = " +sum);
 		System.out.println(digit +" - "+ digit2 +" = " +(digit-digit2));
